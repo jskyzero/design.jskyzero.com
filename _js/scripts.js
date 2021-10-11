a456077937621b0bde7c4f700cd45f3647b49b96
jskyzero <jskyzero@outlook.com>
2021-10-11 18:34:56 +0800

    ✨ add 0.1.png & data-enlargeable

--- _js/scripts.js
+++ _js/scripts.js
@@ -18,6 +18,39 @@ let changeIndexPositon = function () {
 
 changeIndexPositon();
 
+// https://stackoverflow.com/questions/18545077/image-fullscreen-on-click
+
+$('img[data-enlargeable]').addClass('img-enlargeable').click(function() {
+  var src = $(this).attr('src');
+  var modal;
+
+  function removeModal() {
+    modal.remove();
+    $('body').off('keyup.modal-close');
+  }
+  modal = $('<div>').css({
+    background: 'RGBA(0,0,0,.5) url(' + src + ') no-repeat center',
+    backgroundSize: 'contain',
+    width: '100%',
+    height: '100%',
+    position: 'fixed',
+    zIndex: '10000',
+    top: '0',
+    left: '0',
+    cursor: 'zoom-out'
+  }).click(function() {
+    removeModal();
+  }).appendTo('body');
+  //handling ESC
+  $('body').on('keyup.modal-close', function(e) {
+    if (e.key === 'Escape') {
+      removeModal();
+    }
+  });
+});
+
+
+
 // Jquery & Velocity JS included in GULP
 $(document).ready(function () {
   toggleMobileNav();
