d94e744f45587cc3f21219fff0b8416f872e339b
jskyzero <jskyzero@outlook.com>
2021-10-24 21:52:51 +0800

    ✨ finish index order

--- _plugins/jekyll-last-modified-at.rb
+++ _plugins/jekyll-last-modified-at.rb
@@ -10,7 +10,15 @@ module Recents
     def generate(site)
       items = site.posts.docs.select { |p| p.path.end_with? '.md' }
       items.each do |page|
-        page.data['last_modified_at_str'] = Jekyll::LastModifiedAt::Determinator.new(site.source, page.path, '%FT%T%:z').to_s
+        modified_time = Jekyll::LastModifiedAt::Determinator.new(site.source, page.path, '%FT%T%:z').to_s
+        check_time = Time.new(2021,10).strftime('%FT%T%:z')
+        if modified_time > check_time
+          page.data['last_modified_at_str'] = modified_time
+          page.data['use_modified'] = true
+        else
+          page.data['last_modified_at_str'] = check_time
+          page.data['use_modified'] = false
+        end
       end
       puts("FINISH: jekyll-last-modified-at")
     end
